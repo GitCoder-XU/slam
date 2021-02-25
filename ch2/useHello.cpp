3d009b2e2222f579034fade2c1bdb11698ed7cad
GitHubcoder-xu <653061183@qq.com>
2021-02-25 07:58:19 -0800

    slambook配套代码

--- ch2/useHello.cpp
+++ ch2/useHello.cpp
@@ -1,8 +1,11 @@
 #include "libHelloSLAM.h"
+#include <iostream>
+using namespace std;
 
 // 使用 libHelloSLAM.h 中的 printHello() 函数
 int main( int argc, char** argv )
 {
     printHello();
+    cout << "this is a test" << endl;
     return 0;
 }
